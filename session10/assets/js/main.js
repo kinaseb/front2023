6c5839fe4ed37b90fff8ffc43f85b5d4d24e2ba7
mohamed amr <dev.mohamedamr@gmail.com>
2023-09-30 14:55:39 +0300

    10

---
+++ session10/assets/js/main.js
@@ -0,0 +1,24 @@
+// var users = ["name",20,true]
+// console.log( typeof users )
+
+// var user = { name:"mohamed",age:29,address:"giza" }
+// console.log(user.address)
+
+// function hi(name){
+//     console.log("hi "+name)
+// }
+// hi("eslam")
+// hi("tamer")
+// hi("faurouz")
+
+
+
+
+function add(x=1,y=1){
+    return x+y
+}
+
+
+console.log( add(20,20) +40 ) 
+
+
