4c303c4c9a05d41326022591647d3b126feb7c7e
mohamed amr <dev.mohamedamr@gmail.com>
2023-10-21 02:44:34 +0300

    14

--- session14/assets/js/main.js
+++ session14/assets/js/main.js
@@ -2,13 +2,16 @@
 // create , read , update , delete
 
 var saveBtn = document.getElementById("save")
+var updateBtn = document.getElementById("update")
+
 var task  = document.getElementById("task")
 var result = document.getElementById("result")
 var taskStore = []
 
 saveBtn.onclick = function(){
 
-    taskStore.push(task.value)
+    taskStore.push({task:task.value,status:0})
+    console.log(taskStore)
     task.value = ""
     read()
 }
@@ -16,7 +19,11 @@ saveBtn.onclick = function(){
 function read(){
     var text = ""
     for(var i=0;i<taskStore.length;i++){
-        text   += `<li>${taskStore[i]}</li> <button onclick='destroy(${i})'>delete</button> <button onclick='edit(${i})'>edit</button>`
+        if(taskStore[i].status == 0){
+            text   += `<li>${taskStore[i].task}</li> <button onclick='destroy(${i})'>delete</button> <button onclick='edit(${i})'>edit</button> <button onclick='done(${i})'>done</button>`
+        }else {
+            text   += `<li style='text-decoration: line-through'>${taskStore[i].task}</li> <button onclick='destroy(${i})'>delete</button> <button onclick='edit(${i})'>edit</button> <button onclick='done(${i})'>done</button>`
+        }
     }
     result.innerHTML = text
     // text = taskStore.map(function(task){return`<li>${task}</li>`})
@@ -29,10 +36,27 @@ function destroy(index){
     read()
 }
 
+var i;
 
 function edit(index){
-    console.log(index)
+    i = index
+    task.value = taskStore[index].task
+    // console.log()
+    saveBtn.style.display = "none"
+    updateBtn.style.display = "block"
 }
+
+function done(index){
+    taskStore[index].status = 1
+    console.log(taskStore)
+    read()
+}
+
+updateBtn.onclick = function(){
+    taskStore[i].task = task.value
+    read()
+}
+
 
 // var text = "mohamed"
 // text += "eslam"
